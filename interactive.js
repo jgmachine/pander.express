ebe084b6bcb876e4e00a28293eb503698d73ddc5
Jesse <jgmachine@gmail.com>
2024-09-01 19:49:49 -0700

    Update interactive.js

--- interactive.js
+++ interactive.js
@@ -39,8 +39,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 this.x = Math.random() * canvas.width;
                 this.y = Math.random() * canvas.height;
                 this.size = Math.random() * 5 + 1;
-                this.speedX = Math.random() * 2 - 1;
-                this.speedY = Math.random() * 2 - 1;
+                this.speedX = Math.random() * 4 - 2;
+                this.speedY = Math.random() * 4 - 2;
                 this.color = colors[Math.floor(Math.random() * colors.length)];
             }
 
